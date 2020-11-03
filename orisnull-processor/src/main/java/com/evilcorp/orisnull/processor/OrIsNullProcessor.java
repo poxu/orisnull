15fc006db62df9ec963cbe068aedf2afcd734537
poxu <poxvuibr@gmail.com>
2020-11-03 19:09:44 +0300

    Use proper logging system instead of PrintWriter

--- orisnull-processor/src/main/java/com/evilcorp/orisnull/processor/OrIsNullProcessor.java
+++ orisnull-processor/src/main/java/com/evilcorp/orisnull/processor/OrIsNullProcessor.java
@@ -6,6 +6,8 @@ import com.evilcorp.orisnull.model.AnnotatedBetterClass;
 import com.evilcorp.orisnull.model.BetterClass;
 import com.evilcorp.orisnull.model.SearchMethod;
 import com.evilcorp.orisnull.model.SimpleBetterClass;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
@@ -19,9 +21,6 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.ExecutableType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Types;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -31,6 +30,8 @@ import java.util.stream.Collectors;
 @SupportedAnnotationTypes({"com.evilcorp.orisnull.annotation.OrIsNullRepository"})
 @SupportedSourceVersion(SourceVersion.RELEASE_11)
 public class OrIsNullProcessor extends AbstractProcessor {
+    private final Logger logger = LogManager.getLogger(OrIsNullProcessor.class);
+
     private Types types;
 
     @Override
@@ -41,37 +42,30 @@ public class OrIsNullProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        final PrintWriter printWriter;
-        try {
-            printWriter = new PrintWriter("/home/riptor/tmp/debug/sout-" + System.currentTimeMillis() + ".tst");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException();
-        }
-        getSupportedAnnotationTypes().forEach(printWriter::println);
-        printWriter.println("started");
-        printWriter.println("found annotations " + annotations.size());
+        getSupportedAnnotationTypes().forEach(at -> logger.debug(() -> at));
+        logger.debug(() -> "started");
+        logger.debug(() -> "found annotations " + annotations.size());
         for (TypeElement annotation : annotations) {
             Set<? extends Element> annotatedElements
                     = roundEnv.getElementsAnnotatedWith(annotation);
-            printWriter.println("found classes " + annotatedElements.size());
+            logger.debug(() -> "found classes " + annotatedElements.size());
             for (Element annotatedElement : annotatedElements) {
 
-                printWriter.println("found class " + annotatedElement.getSimpleName());
+                logger.debug(() -> "found class " + annotatedElement.getSimpleName());
                 final List<? extends Element> collect = annotatedElement.getEnclosedElements()
                         .stream()
                         .filter(e -> e.getKind() == ElementKind.METHOD)
                         .collect(Collectors.toList());
-                printWriter.println("forund methods " + collect.size());
+                logger.debug(() -> "forund methods " + collect.size());
+
                 BetterClass iface = new AnnotatedBetterClass(annotatedElement);
 
-                //Element method = collect.iterator().next();
                 List<SearchMethod> methods = new ArrayList<>();
 
                 for (Element method : collect) {
-
-                    printWriter.println(method.toString());
-                    printWriter.println(method.getEnclosedElements().size());
-                    printWriter.println(method.getKind());
+                    logger.debug(() -> method.toString());
+                    logger.debug(() -> method.getEnclosedElements().size());
+                    logger.debug(() -> method.getKind());
 
                     final var typeMirror = (ExecutableType) method.asType();
                     if (typeMirror.getParameterTypes().size() != 1) {
@@ -83,26 +77,27 @@ public class OrIsNullProcessor extends AbstractProcessor {
                     if (query == null) {
                         continue;
                     }
-                    printWriter.println(query.value());
+
+                    logger.debug(() -> query.value());
 
                     final TypeMirror paratemeter = typeMirror.getParameterTypes().iterator().next();
                     BetterClass filter = new AnnotatedBetterClass(processingEnv.getTypeUtils().asElement(paratemeter));
 
                     filter.fields().forEach(f -> {
-                        printWriter.println(f.name());
-                        printWriter.println(f.type());
+                        logger.debug(() -> f.name());
+                        logger.debug(() -> f.type());
                     });
 
-                    printWriter.println(typeMirror.getReturnType());
+                    logger.debug(() -> typeMirror.getReturnType());
 
                     final var returnTypeName = typeMirror.getReturnType().toString();
 
-                    if (!returnTypeName.startsWith("java.util.List")) {
+                    if (!returnTypeName.startsWith("java.util.List<")) {
                         continue;
                     }
 
                     final var book = returnTypeName.replaceAll("^.*<(.*)>", "$1");
-                    printWriter.println(book);
+                    logger.debug(() -> book);
                     BetterClass entity = new SimpleBetterClass(book, Collections.emptyList());
 
                     methods.add(new SearchMethod(
@@ -113,34 +108,13 @@ public class OrIsNullProcessor extends AbstractProcessor {
                     ));
                 }
 
-
-                /*
-                typeMirror.getParameterTypes().forEach(
-                        p -> {
-                            printWriter.println(p.toString());
-                            processingEnv.getTypeUtils().asElement(p).getEnclosedElements()
-                                    .stream()
-                                    .filter(e -> e.getKind() == ElementKind.FIELD)
-                                    .forEach(e -> {
-                                        printWriter.println(e.toString());
-                                        printWriter.println(e.asType().toString());
-                                        final var simpleName = processingEnv.getTypeUtils().asElement(e.asType())
-                                                .getSimpleName();
-                                        printWriter.println(simpleName);
-                                    });
-                        });
-                */
-                //annotatedElement.
-                //processingEnv.getFiler();
                 final var searchGenerator = new SearchServiceGenerator(
                         iface,
                         methods
                 );
-                searchGenerator.toFile(new File("/home/riptor/tmp/"));
                 searchGenerator.toFiler(processingEnv.getFiler());
             }
         }
-        printWriter.close();
         return true;
     }
 }
